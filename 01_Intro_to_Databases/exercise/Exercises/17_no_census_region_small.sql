400627d2ab2640eaa67b0f98444b0b354fd2540d
Jennifer Brown <jenbrown1983@gmail.com>
2022-04-23 20:04:57 -0500

    Finished

--- 01_Intro_to_Databases/exercise/Exercises/17_no_census_region_small.sql
+++ 01_Intro_to_Databases/exercise/Exercises/17_no_census_region_small.sql
@@ -1,2 +1,6 @@
 -- 17. The name, population, and area of all records in the state table with no census region (NULL) and area less than 5000 square kilometers (3 rows)
+Select state_name, population, area
+from state
+where area < 5000 and census_region is null;
+
 
