47353ec1b0aff93cddffdd64127c8a663d56b79f
Jennifer Brown <jenbrown1983@gmail.com>
2022-06-01 19:26:46 -0500

    done

--- 16_Securing_APIs/exercise/client/src/main/java/com/techelevator/auctions/services/AuthenticationService.java
+++ 16_Securing_APIs/exercise/client/src/main/java/com/techelevator/auctions/services/AuthenticationService.java
@@ -2,6 +2,10 @@ package com.techelevator.auctions.services;
 
 import com.techelevator.util.BasicLogger;
 import org.springframework.http.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestClientResponseException;
 import org.springframework.web.client.RestTemplate;
@@ -21,7 +25,12 @@ public class AuthenticationService {
         HttpEntity<CredentialsDto> entity = new HttpEntity<>(credentialsDto, headers);
         String token = null;
         try {
-            // Add code here to send the request to the API and get the token from the response.
+            ResponseEntity<TokenDto> response = restTemplate.exchange(API_BASE_URL +
+                    "login", HttpMethod.POST, entity, TokenDto.class);
+            TokenDto body = response.getBody();
+            if(body != null){
+                token = body.getToken();
+            }
         } catch (RestClientResponseException | ResourceAccessException e) {
             BasicLogger.log(e.getMessage());
         }
